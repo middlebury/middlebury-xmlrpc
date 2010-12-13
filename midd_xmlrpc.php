60a365ab0ef7b84a139a77784bb6c0c0df043b96
Adam Franco <afranco@middlebury.edu>
2010-12-13 14:29:50 -0500

    Can now return information from a single blog and create new blogs via XML-RPC.

--- midd_xmlrpc.php
+++ midd_xmlrpc.php
@@ -14,6 +14,8 @@ function midd_xmlrpc_methods( $methods ) {
 	'midd.blogExists' => 'midd_xmlrpc_blogExists',
 	'midd.getUsersBlogs' => 'midd_xmlrpc_getUsersBlogs',
 	'midd.searchBlogs' => 'midd_xmlrpc_searchBlogs',
+	'midd.getBlog' => 'midd_xmlrpc_getBlog',
+	'midd.createBlog' => 'midd_xmlrpc_createBlog',
     ));
 }
 
@@ -64,12 +66,35 @@ function midd_xmlrpc_blogExists ($name) {
 	return !is_null($id);
 }
 
+
+/**
+ * Answer information about a blog. Answer false if doesn't exist.
+ *
+ * @param string $name
+ *      The first a blog name.
+ * @return mixed array or FALSE
+ */
+function midd_xmlrpc_getBlog ($name) {
+	if (!strlen($name))
+		return(new IXR_Error(400, __("This method takes one parameter, a blog name string.")));
+
+	// WPCAS always redirects to /wp-admin/ after login, so we need to call our
+	// own login function
+	$user = midd_xmlrpc_authenticate();
+
+	$id = get_id_from_blogname($name);
+	if(is_null($id))
+		return false;
+	return midd_xmlrpc_blogInfo($id);
+}
+
+
 /**
  * search for blogs
  *
  * @param string $query
  *      The search query.
- * @return boolean
+ * @return array
  */
 function midd_xmlrpc_searchBlogs ($query) {
 	if (!strlen($query))
@@ -91,6 +116,61 @@ function midd_xmlrpc_searchBlogs ($query) {
 
 	return $blogInfo;
 }
+
+/**
+ * Create a new blog
+ *
+ * @param string $args '
+ * @return string
+ */
+function midd_xmlrpc_createBlog ($args) {
+	if (!is_array($args) || count($args) != 3)
+		return(new IXR_Error(400, __("This method requires 3 parameters, a name, a title, and a visibility setting.")));
+	$name = $args[0];
+	$title = $args[1];
+	$public = $args[2];
+	if (!strlen($name))
+		return(new IXR_Error(400, __("This method requires a blog name string.")));
+	if (!strlen($title))
+		return(new IXR_Error(400, __("This method requires a blog title string.")));
+	if (!is_int($public))
+		return(new IXR_Error(400, __("This method requires an integer visibility parameter.")));
+
+	// WPCAS always redirects to /wp-admin/ after login, so we need to call our
+	// own login function
+	$user = midd_xmlrpc_authenticate();
+
+	// Create the blog (based on validate_blog_signup() in wp-signup.php)
+	$result = wpmu_validate_blog_signup($name, $title, $user);
+	extract($result);
+	if ($errors->get_error_code()) {
+		return(new IXR_Error(400, $errors->get_error_message()));
+	}
+
+// 	return(new IXR_Error(400, "<pre>".print_r($result, true)."</pre>"));
+
+	$meta = array ('lang_id' => 1, 'public' => $public);
+	$meta = apply_filters( "add_signup_meta", $meta );
+
+	wpmu_signup_blog($domain, $path, $blog_title, $user->user_login, $user->user_email, $meta);
+
+	ob_start();
+	print "<p style='font-weight: bold; font-size: larger;'>";
+	printf( __( 'Congratulations! Your new site, %s, is almost ready.' ), "<a href='http://{$domain}{$path}'>{$blog_title}</a>" );
+	print "</p>";
+	print "<p>";
+	_e( 'But, before you can start using your site, <strong>you must activate it</strong>.' );
+	print "</p>";
+	print "<p>";
+	printf( __( 'Check your inbox at <strong>%s</strong> and click the link given.' ),  $user->user_email);
+	print "</p>";
+	print "<p>";
+	_e( 'If you do not activate your site within two days, you will have to sign up again.' );
+	print "</p>";
+
+	return ob_get_clean();
+}
+
 
 /**
  * Authenticate the user.
